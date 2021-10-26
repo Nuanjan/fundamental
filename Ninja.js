a4ba1fbf31a737ead7a72ed132aa7cb693bc37db
Nuanjan <nuanjan.schluntz@gmail.com>
2021-10-25 22:59:37 -0400

    Add Sensei class and extends Ninja class

--- Ninja.js
+++ Ninja.js
@@ -1,23 +1,48 @@
-
 class Ninja {
-    constructor(name, health=0, speed=3,strength=3){
-        this.name = name;
-        this.health = health;
-        this.speed = speed;
-        this.strength = strength;
-    }
-    sayName(){
-       console.log("Ninja Name: ",this.name); 
-    }
-    showStats(){
-        console.log("Ninja Name: ", this.name, " Strength: ", this.strength, " Health: ", this.health);
-    }
-    drinkSake(){
-        this.health += 10;
-    }
+  constructor(name) {
+    this.name = name;
+    this.health = 0;
+    this.speed = 3;
+    this.strength = 3;
+  }
+  sayName() {
+    console.log("Ninja Name: ", this.name);
+  }
+  showStats() {
+    console.log(
+      "Ninja Name: ",
+      this.name,
+      " Strength: ",
+      this.strength,
+      " Health: ",
+      this.health
+    );
+  }
+  drinkSake() {
+    this.health += 10;
+  }
 }
-const ninja1 = new Ninja("Hyabusa");
-ninja1.sayName();
-ninja1.showStats();
-ninja1.drinkSake();
-ninja1.showStats();
+
+class Sensei extends Ninja {
+  constructor(name) {
+    super(name);
+    this.wisdom = 10;
+    this.health = 200;
+    this.speed = 10;
+    this.strength = 10;
+  }
+  speakWisdom() {
+    this.drinkSake();
+    console.log(
+      "What one programmer can do in one month, two programmers can do in two months."
+    );
+  }
+}
+// const ninja1 = new Ninja("Hyabusa");
+// ninja1.sayName();
+// ninja1.showStats();
+// ninja1.drinkSake();
+// ninja1.showStats();
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+superSensei.showStats();
